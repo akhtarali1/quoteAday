aeef48bfd81ffb13e7765090b4d3089bc32cbced
Akhtar Ali <Akhtar.Ali2@klm.com>
2018-03-05 14:50:45 +0100

    removed file system from app

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var fileSystem = require("fs");
+// var fileSystem = require("fs");
 var app = express();
 app.use(express.static('./view'))
-var text = fileSystem.readFileSync("./files/quotes.txt", "utf-8");
-var textByLine = text.split("\n");
+// var text = fileSystem.readFileSync("./files/quotes.txt", "utf-8");
+// var textByLine = text.split("\n");
 
 app.get('/', function(request, response) {
-	var quote = textByLine[Math.floor(Math.random() * textByLine.length)];
-	response.render('index.html', {name : quote});
+	// var quote = textByLine[Math.floor(Math.random() * textByLine.length)];
+	response.render('index.html');
 });
 
 var port = process.env.PORT || 8088;
